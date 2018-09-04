39d3410651cfa3ff0a94d0d32dc827db548a4705
rita <m.tsepiennikova@mail.com>
2018-09-04 16:09:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        SimpleXMLParser xml = new SimpleXMLParser("c:\\1.xml");
+        String value = xml.get("catalog/book/author");
+        System.out.println(value);
+    }  
+}
